332fd9e04146d19a4ba5f03e300346497c704e2a
Thiago Culau <thiago.culau1@outlook.com>
2024-04-03 00:29:46 -0300

    Add project files.

---
+++ TorneioDeTenis/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace TorneioDeTenis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cont = 0, aux = 0;
+            while (aux < 6)
+            {
+                aux++;
+                string vitoria = Console.ReadLine();
+                if (vitoria != "V" && vitoria != "v" && vitoria != "p" && vitoria != "P")
+                {
+                    Console.WriteLine("Comando invalido. Tente novamente. Precione qualquer tecla para continuar");
+                    Console.ReadLine();
+                    Console.Clear();
+                    continue;
+                }
+                if (vitoria == "v" || vitoria == "V")
+                {
+                    cont++;
+                }
+            }
+            int resultado = 0;
+            if (cont == 1 || cont == 2)
+            {
+                resultado = 3;
+            }
+            if (cont == 3 || cont == 4)
+            {
+                resultado = 2;
+            }
+            if (cont == 5 || cont == 6)
+            {
+                resultado = 1;
+            }
+            if (cont < 1)
+            {
+                resultado = -1;
+            }
+            Console.Clear();
+            Console.Write(resultado);
+            Console.ReadLine();
+        }
+    }
+}
